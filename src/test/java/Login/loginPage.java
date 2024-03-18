1f3d552783ee9fc4f3b1479bb5d7e20f2fa0bc5f
lapto <asmahnh95@gmail.com>
2024-03-18 11:05:31 +0800

    Update yml build to Java 17

---
+++ src/test/java/Login/loginPage.java
@@ -0,0 +1,35 @@
+package Login;
+
+import Intro.openBrowser;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class loginPage extends openBrowser {
+
+    @Test
+    public void loginTest(){
+        driver.get(baseURL);
+
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        usernameField.sendKeys(username);
+
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys(password);
+
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+
+        //Verify Account Login
+        if (driver.getCurrentUrl().equals("https://www.saucedemo.com/inventory.html"))
+        {
+           Assert.assertTrue(true);
+           System.out.println("Login Successful");
+        }
+        else {
+            Assert.assertFalse(false, "Login Failed");
+            System.out.println("Epic sadface: Username and password do not match any user in this service");
+        }
+    }
+}
