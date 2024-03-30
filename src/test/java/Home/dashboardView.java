8ef2369e56ade0a2af7512ad0fdd1a860283df65
lapto <asmahnh95@gmail.com>
2024-03-30 23:14:34 +0800

    Add Dashboard View Script

--- src/test/java/Home/dashboardView.java
+++ src/test/java/Home/dashboardView.java
@@ -15,13 +15,23 @@ import java.time.Duration;
 public class dashboardView extends loginPage {
 
     @Test(priority = 2)
-    public void productView(){
+    public void productView1(){
 
         WebDriverWait wait_Backpack = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement sauceLabBackpack = wait_Backpack.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/a/div")));
         String actualTitle_Backpack=sauceLabBackpack.getText();
         String expectedTitle_Backpack = "Sauce Labs Backpack";
         Assert.assertEquals(actualTitle_Backpack, expectedTitle_Backpack, "Title does match");
-        System.out.println("sauceLabBackpack");
+        System.out.println("sauce Labs Backpack");
+    }
+
+    @Test(priority = 3)
+    public void productView2(){
+        WebDriverWait wait_Bikelight = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement sauceLabBikelight = wait_Bikelight.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[2]/div[2]/div[1]/a/div")));
+        String actualTitle_Bikelight=sauceLabBikelight.getText();
+        String expectedTitle_Bikelight = "Sauce Labs Bike Light";
+        Assert.assertEquals(actualTitle_Bikelight, expectedTitle_Bikelight, "Title does match");
+        System.out.println("Sauce Labs Bike Light");
     }
 }
