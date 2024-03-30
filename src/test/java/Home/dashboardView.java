5cc973d15298911351dba3a5c66912ba5bdd7ef2
lapto <asmahnh95@gmail.com>
2024-03-30 23:19:32 +0800

    Update Dashboard View Script

--- src/test/java/Home/dashboardView.java
+++ src/test/java/Home/dashboardView.java
@@ -15,23 +15,35 @@ import java.time.Duration;
 public class dashboardView extends loginPage {
 
     @Test(priority = 2)
-    public void productView1(){
+    public void productView1() {
 
         WebDriverWait wait_Backpack = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement sauceLabBackpack = wait_Backpack.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/a/div")));
-        String actualTitle_Backpack=sauceLabBackpack.getText();
+        String actualTitle_Backpack = sauceLabBackpack.getText();
         String expectedTitle_Backpack = "Sauce Labs Backpack";
         Assert.assertEquals(actualTitle_Backpack, expectedTitle_Backpack, "Title does match");
         System.out.println("sauce Labs Backpack");
     }
 
     @Test(priority = 3)
-    public void productView2(){
+    public void productView2() {
         WebDriverWait wait_Bikelight = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement sauceLabBikelight = wait_Bikelight.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[2]/div[2]/div[1]/a/div")));
-        String actualTitle_Bikelight=sauceLabBikelight.getText();
+        String actualTitle_Bikelight = sauceLabBikelight.getText();
         String expectedTitle_Bikelight = "Sauce Labs Bike Light";
         Assert.assertEquals(actualTitle_Bikelight, expectedTitle_Bikelight, "Title does match");
         System.out.println("Sauce Labs Bike Light");
     }
+
+    @Test(priority = 4)
+    public void productView3() {
+        WebDriverWait wait_Bolttshirt = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement sauceLabBolttshirt = wait_Bolttshirt.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[3]/div[2]/div[1]/a/div")));
+        String actualTitle_Bolttshirt = sauceLabBolttshirt.getText();
+        String expectedTitle_Bolttshirt = "Sauce Labs Bolt T-Shirt";
+        Assert.assertEquals(actualTitle_Bolttshirt, expectedTitle_Bolttshirt, "Title does match");
+        System.out.println("Sauce Labs Bolt T-Shirt");
+
+    }
+
 }
